b2896a6c0e156315a9b42c817f50626ce716b2fa
“nerdale” <alexandra.neira@gmail.com>
2016-12-28 16:28:40 -0300

    fixed dependencies

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+var gulp = require('gulp');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var jshint = require('gulp-jshint');
+var sass = require('gulp-sass');
+var minifyCSS = require('gulp-minify-css');
+
+gulp.task('script', function(){
+
+	gulp.src('src/js/*.js')
+		.pipe(jshint())
+		.pipe(jshint.reporter('default'))
+		.pipe(concat('script.min.js'))
+		.pipe(uglify())
+		// carpeta dist
+		.pipe(gulp.dest('dist/js'));
+});
+
+gulp.task('style', function(){
+	gulp.src('src/sass/main.scss')
+		.pipe(sass().on('error', sass.logError))
+		.pipe(minifyCSS())
+		.pipe(concat('style.min.css'))
+		.pipe(gulp.dest('dist/css'));
+});
+
+
+gulp.task('default', ['script', 'style']);
